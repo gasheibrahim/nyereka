8c94c3af2c0602d074c75080d50a49eeec942eea
gasheibrahim <ibrahimgashema154@gmail.com>
2020-08-25 10:49:57 +0200

    delete checkout on line iteml

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,6 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
-  has_many :checkouts
   has_one :order, dependent: :destroy 
   def total_price
     product.sales_price.to_i * quantity.to_i

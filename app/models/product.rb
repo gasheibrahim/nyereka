4967b47512c81fe31d93eb3c0fa08e6c96638bdd
gasheibrahim <ibrahimgashema154@gmail.com>
2020-08-26 10:56:55 +0200

    validate on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,18 @@ class Product < ApplicationRecord
   has_many :line_items
   has_many :sub_categories
   has_many :sub_sub_categories
+  validates :product_name, presence: true
+  validates :product_image, presence: true
+  validates :product_details, presence: true
+  validates :specification, presence: true
+  validates :package, presence: true
+  validates :sales_price, presence: true
+  validates :regular_price, presence: true
+  validates :stock, presence: true
+  validates :location, presence: true
+  validates :shop, presence: true
+  validates :rent_used, presence: true
+  validates :images, presence: true
   has_many_attached :images
   mount_uploader :product_image, ImageUploader
   scope :product_name_like, -> (product_name) { where("product_name ilike ?", product_name)}
